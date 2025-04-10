c75824cccd065617056eea4f14386fbe5c6a3125
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-10 17:21:50 +0200

    messa appunto struttura

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 const nameInput = document.getElementById("name-input");
 const distanceInput = document.getElementById("distance-input");
 const ageInput = document.getElementById("age-input");
+const registredPersonForm = document.getElementById("register-person-form");
 
-
-const sendButton = document.getElementById("btn-generate");
-
-sendButton.addEventListener("click",function (){
+registredPersonForm.addEventListener("submit",function (){
     const name = nameInput.value;
     const distance = distanceInput.value;
     const age = parseInt(ageInput.value);
 
-    console.log("Nome persona:" + name);
-    console.log("km da percorrere:" +  distance);
-    console.log("fascia d'età" + age);
+    console.log("Nome e Cognome:" + name);
+    console.log("Km da percorrere:" +  distance);
+    console.log("Fascia d'età:" + age);
     
 
     
