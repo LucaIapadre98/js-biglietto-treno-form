edc32a4803c4e20b65a5f34f9e93da10d6de895d
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-10 16:30:19 +0200

    aggiunto un po di js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+const nameInput = document.getElementById("name-input");
+const distanceInput = document.getElementById("distance-input");
+const ageInput = document.getElementById("age-input");
 
+
+const sendButton = document.getElementById("btn-generate");
+
+sendButton.addEventListener("click",function (){
+    const name = nameInput.value;
+    const distance = distanceInput.value;
+    const age = parseInt(ageInput.value);
+
+    console.log("Nome persona:" + name);
+    console.log("km da percorrere:" +  distance);
+    console.log("fascia d'età" + age);
+    
+
+    
+})
